14f5aed2efbb079752babe5179da6350f2d55516
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-15 22:26:21 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(args = {title: , release_date: })
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = 'The Room'
-    m.release_date = 1990
+    m.title
+    m.release_date
   end
 end
 
