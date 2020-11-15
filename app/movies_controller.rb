dbae3922c54dc3d58763b201a225f769b0dab96a
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-15 22:01:49 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(args = {})
   # release_date == 1990
 
   Movie.create do |m|
-    u.title = 'Home Alone'
-    u.release_date = 1990
+    m.title = 'Home Alone'
+    m.release_date = 1990
   end
 end
 
